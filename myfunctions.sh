f0b0504b3c08bb6e9e6613b575167830b69c16ad
Tillman Dickson, Jr <tillman_jr@yahoo.com>
2020-02-28 14:05:18 -0800

    add iTerm2 Title functions

--- myfunctions.sh
+++ myfunctions.sh
@@ -12,3 +12,31 @@ function mkcd() { mkdir "$1" && cd "$1"; }
 # ----------------------
 # Git log find by commit message
 function gtlgf() { git log --all --grep="$1"; }
+
+# ----------------------
+# Docker Functions
+# ----------------------
+# Prune everything - should add confirmation
+function dkrpga() { docker system prune --all; }
+function dkrcls() { docker container ls;}
+function dkrcis() { docker image ls; }
+function dkcbsh() { docker-compose exec "$1" bash; }
+
+# ----------------------
+# iTerm2 Functions
+# ----------------------
+# Setup tab and window title functions for iterm2
+# iterm behaviour: until window name is explicitly set, it'll always track tab title.
+# So, to have different window and tab titles, iterm_window() must be called
+# first. iterm_both() resets this behaviour and has window track tab title again).
+# Source: http://superuser.com/a/344397
+set_iterm_name() {
+  mode=$1; shift
+  echo -ne "\033]$mode;$@\007"
+}
+iterm_both () { set_iterm_name 0 $@; }
+iterm_tab () { set_iterm_name 1 $@; }
+iterm_window () { set_iterm_name 2 $@; }
+
+
+
